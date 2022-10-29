dce34418059b5e0d8c3dd0feb8b9edd1cd883bc3
FluoriteFire <Fluorite_fire@outlook.com>
2022-10-29 17:06:33 +0800

    增加结束后分数

--- snakes.cpp
+++ snakes.cpp
@@ -11,12 +11,15 @@ int main() {
 	vector <vector<int> > map(HEIGHT, vector<int> (WEIGHT));
 	vector <point> apple;
 	snake s;
+	bool flag = true;
+	int scroe = 0;
+
 	clear(map); 
 	print(map);
 	s.draw(map);
 	
 
-	while(1){
+	while(flag){
 		if(kbhit()){
 			ch = getch();
 		}
@@ -25,8 +28,14 @@ int main() {
 		if((double)(time - last_time)/CLOCKS_PER_SEC >= s.speed){
 			last_time = time;
 		  	s.turn(ch);
-			s.eat(map,apple); 
+			switch(s.eat(map,apple)){
+				case 1:	flag = false;system("cls");cout<<"你的分数是："<<scroe;break;
+				case 2:	flag = false;system("cls");cout<<"你的分数是："<<scroe;break;
+				case 4:	++scroe;break;
+				default:break;
+			}
 		}
 	}
+	getchar();
 	return 0;
 }
